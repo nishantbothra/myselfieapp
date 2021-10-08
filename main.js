a27fb95e1c320611ea46f26e7d99e8e498f35cc2
nishantbothra <83067785+nishantbothra@users.noreply.github.com>
2021-10-08 15:27:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+recognition.onresult = function(event) {
+    console.log(event);
+    var content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = content;
+    speak();
+}
+function speak() {
+    var synth = window.SpeechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 100
+});
+camera = document.getElementById("my_webcam");
+Webcam.attach(camera);
+
+
+
+
+
+
+
+
